dca37cf942bb038c1ef143769d3652139b26cd15
Artem <d00m7@ukr.net>
2018-08-14 14:00:54 +0300

    Template integration

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-        $products = [
-            0 => ['name' => 'Iphone', 'category' => 'smart phones', 'price' => 1000],
-            1 => ['name' => 'Galaxy', 'category' => 'tablets', 'price' => 2000],
-            2 => ['name' => 'Sony', 'category' => 'TV', 'price' => 3000],
-        ];
+        $products = Product::all();
+
         return view('allproducts', compact('products'));
     }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'description', 'price', 'image', 'type'
+    ];
+
+    public function getPriceAttribute($value)
+    {
+        return "$".$value;
+    }
+
 }
